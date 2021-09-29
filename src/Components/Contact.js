3cd9b759e7def17a8314ca979e7846be3a685657
Azhar Kalarickal <48583694+Azharkalarickal@users.noreply.github.com>
2021-09-29 21:02:33 +0530

    contact bootstrap

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,75 +1,76 @@
 import React from 'react'
+import { Row,Col } from 'react-bootstrap'
 import './Contact.css'
 function Contact() {
     return (
-        <div className="row">
-        <div className="col-12 col-lg-6">
-        <div className="container-contact" id="contact-section">
-        <div className="form">
-            <p className="title-contact ">Contact form</p>
-            <p className="para-contact">Drop a message !</p>
-            <p className="para1-contact">Fields marked with an asterisk (*) are required.</p>
-            <form>
-                <div className="form-container ">
-                <table className="hai">
-                    <tbody>
-                        <tr >
-                            <td>
-                                <div className="text-contact">First name*</div>
-                            </td>
-                            <td className="colm-contact">
-                            <div className="text-contact">Last name*</div>
-                            </td>
-                        </tr>
-                        <tr >
-                            <td>
-                                <input className="box-contact " type="text" />
-                            </td>
-                            <td className="colm-contact">
-                                <input className="box-contact" type="text" />
-                            </td>
-                        </tr>
-                        <tr >
-                            <td>
-                                <div className="text-contact">Email*</div>
-                            </td>
-                            <td className="colm-contact">
-                            <div className="text-contact">Phone number</div>
-                            </td>
-                        </tr>
-                        <tr >
-                            <td>
-                                <input className="box-contact" type="text" />
-                            </td>
-                            <td className="colm-contact" >
-                                <input className="box-contact" type="text" />
-                            </td>
-                        </tr>
-                        
-                        
-                    </tbody>
-                </table>
-                <br/>
-                <div className="message-text">
-                <div className="text-contact">Message</div>
+        <Row>
+            
+               
+                    <Col xs={12} lg={8} className="form">
+                        <p className="title-contact ">Contact form</p>
+                        <p className="para-contact">Drop a message !</p>
+                        <p className="para1-contact">Fields marked with an asterisk (*) are required.</p>
+                        <form>
+                            <div className="form-container ">
+                                <table className="hai">
+                                    <tbody>
+                                        <tr >
+                                            <td>
+                                                <div className="text-contact">First name*</div>
+                                            </td>
+                                            <td className="colm-contact">
+                                                <div className="text-contact">Last name*</div>
+                                            </td>
+                                        </tr>
+                                        <tr >
+                                            <td>
+                                                <input className="box-contact " type="text" />
+                                            </td>
+                                            <td className="colm-contact">
+                                                <input className="box-contact" type="text" />
+                                            </td>
+                                        </tr>
+                                        <tr >
+                                            <td>
+                                                <div className="text-contact">Email*</div>
+                                            </td>
+                                            <td className="colm-contact">
+                                                <div className="text-contact">Phone number</div>
+                                            </td>
+                                        </tr>
+                                        <tr >
+                                            <td>
+                                                <input className="box-contact" type="text" />
+                                            </td>
+                                            <td className="colm-contact" >
+                                                <input className="box-contact" type="text" />
+                                            </td>
+                                        </tr>
+
+
+                                    </tbody>
+                                </table>
+                                <br />
+                                <div className="message-text">
+                                    <div className="text-contact">Message</div>
+
+                                    <textarea className="message-box" type="text" />
+                                    <div>
+                                    </div>
+                                    <button className="btn-contact">Submit Message</button>
+                                </div>
+                            </div>
+                        </form>
+                    </Col>
+                    <Col xs={12} lg={4} className="adress">
+                        <p className="getin">Get In Touch</p>
+                        <p className="con-adress">www.lantespera.com </p><br />
+                        <p className="getin2">Follow Us In</p>
+                        <p className="con-adress2">facebook<br />instagram<br />youtube </p>
+                    </Col>
                 
-                <textarea className="message-box" type="text" />
-                <div>
-                </div>
-                <button className="btn-contact">Submit Message</button>
-                </div>
-            </div>
-            </form>
-            </div>
-                <div className="adress">
-                    <p className="getin">Get In Touch</p>
-                    <p className="con-adress">www.lantespera.com </p><br/>
-                    <p className="getin2">Follow Us In</p>
-                    <p className="con-adress2">facebook<br/>instagram<br/>youtube </p>
-                </div>
-            </div> 
-        </div>
-        </div>
+            
+        </Row>
     )
 }
 export default Contact
